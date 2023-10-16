a066a66ee9e501142a3a37fe8a1dc281cc8154cd
Ayman Edris <ayman.edris1986@gmail.com>
2023-10-16 14:39:58 +0300

    adding multi subscriptions using ConnectableFlux

--- demo/core-eng-demo/src/main/java/com/n/rwf/flux/RequestHandler.java
+++ demo/core-eng-demo/src/main/java/com/n/rwf/flux/RequestHandler.java
@@ -1,17 +1,31 @@
 package com.n.rwf.flux;
 
 import com.n.rwf.flux.ConsumerFluxSink;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 
 public class RequestHandler {
     Flux<String> mainTube;
+    ConnectableFlux<String> subTube;
     private ConsumerFluxSink<String> fs;
     public RequestHandler(){
         fs = new ConsumerFluxSink<>();
         mainTube = Flux.create(fs);
-        mainTube.subscribe(s -> {
-            System.out.println("n sink "+s);
+        subTube = mainTube.publish();
+
+
+        subTube.subscribe(s -> {
+            System.out.println("n sink 1 "+s);
         });
+        subTube.filter( s -> s != null && s.contains("hello")).subscribe(
+                s -> {
+                    System.out.println(" hello baaack ");
+                }
+        );
+        subTube.subscribe(s -> {
+            System.out.println("n sink 2 "+s);
+        });
+        subTube.connect();
     }
 
     public void startProcess(String startAction){
